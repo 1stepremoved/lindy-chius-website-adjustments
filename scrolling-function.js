07eb2673aa799311bc6beaea7a622d22c399085c
Hamilton Sands <hamilton.sands@publicismedia.com>
2020-02-25 21:46:22 -0500

    remove scroll function entirely

--- scrolling-function.js
+++ scrolling-function.js
@@ -9,63 +9,6 @@ $.fn.extend({
       }
       return y
   },
-  scrolling: function() {
-    var t = true,
-        y = $(this).scrollTop(),
-        w = $(window).height();
-    $(window).resize(function(){
-        w = $(window).height();
-    });
-
-    const slideAndWaitWrapper = (slideFunction) => () => {
-        if (t) {
-            slideFunction();
-            t = false;
-            setTimeout(function(){ t = true }, 1200);
-        }
-    };
-
-    const slideDown = () => {
-        y = $(this).move(y, w);
-    };
-
-    const slideUp = () => {
-        y = $(this).move(y, -w);
-    };
-
-    const slideDownAndWait = slideAndWaitWrapper(slideDown);
-    const slideUpAndWait = slideAndWaitWrapper(slideUp);
-
-    $(this).bind('mousewheel', function(event){
-        var delta = event.deltaY;
-        if (delta < 0)
-            slideDownAndWait();
-        else if (delta > 0)
-            slideUpAndWait();
-    });
-
-    var ts;
-    $(this).bind('touchstart', function (e){
-        ts = e.originalEvent.touches[0].clientY;
-    });
-
-    $(this).bind('touchend', function (e){
-        var te = e.originalEvent.changedTouches[0].clientY;
-        if(ts > te+5){
-            slideDownAndWait();
-        }else if(ts < te-5){
-            slideUpAndWait();
-        }
-    });
-
-    $(this).on('keydown', function(event){
-        var k = event.which;
-        if (k == 32 || k == 74 || k == 40)
-            slideDownAndWait();
-        else if (k == 75 || k == 38)
-            slideUpAndWait();
-    });
-  },
   slide: function() { // photoset slideshows
       $(this).find('.photo:first-child').addClass('active');
       $(this).find('.photo').click(function(){
@@ -93,7 +36,6 @@ $(document).ready(function(){
   var color = '#ededed',
       $document = $(document),
       $content = $('.content');
-  $document.scrolling();
   $content.slide();
   $content.plyrs(color);
   $('.photo-slideshow').pxuPhotoset({
